6bf8137b1c70331f6f676c6f504298ef6ff6a3af
mpericles <mpericles1@yahoo.com>
2023-07-11 18:54:14 -0400

    LINQ-Exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+            //create a list of string .Orderby
+            List<string> VideoGames = new List<string>()
+            {
+                "Gameboy",
+                "Tetris",
+                "Pacman",
+                "Candycrush"
+            };
+            VideoGames.Add("joystick");
+            //populate the list .Add
+            VideoGames.Add("joystick");
+            VideoGames.Add("toystick");
+
+            //sort the list by string lenght . lenght
+            Console.WriteLine();
+            Console.WriteLine("List Sorted by the Length of the VideoGame Name");
+            var SortedVideoGames = VideoGames.OrderBy(x => x.Length);
+            foreach ( var videoGame in SortedVideoGames)
+            { Console.WriteLine(videoGame); };
+
+
         }
     }
 }
